7a723d3094aaa4b0ee2352b57a291a6ade2b7190
Magnus Rembold <magnus.rembold@zhdk.ch>
2014-09-18 08:47:44 +0200

    Neues Beispiel für nachgeladene AJAX Seiten

--- aufgabe_02_loesung_a/js/main.js
+++ aufgabe_02_loesung_a/js/main.js
@@ -2,6 +2,7 @@ $( document ).ready( function() {
   console.log( "Hallo los geht's!" );
   
   // Suche nach der Menu Taste
+  var $menu;
   $menu = $( '.menubutton' );
   $menu.click( function( e ) {
     console.log( '> click @ .menubutton' );
