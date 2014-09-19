32ab304a7ab0dd3161fdae4b40de8347001894a9
Magnus Rembold <magnus.rembold@zhdk.ch>
2014-09-19 08:09:46 +0200

    Funktionen entzerrt, Variablen eingesetzt

--- ajax_pages_simple/js/main.js
+++ ajax_pages_simple/js/main.js
@@ -1,33 +1,43 @@
+function f_page_slide( e ) {
+  console.log( '> complete @ $seite.load' );
+  // Seite ist geladen worden, starte Slide Animation
+  $seite.removeClass( 'out_right' );
+  $( '.page#haupt' ).addClass( 'out_left' );
+  $( '.menubutton#back' ).show();
+}
+
+function f_page_slide_back( e ) {
+  console.log( '> complete @ $seite.load' );
+  // Seite ist geladen worden, starte Slide Animation
+  $seite.removeClass( 'out_right' );
+  $( '.page#haupt' ).addClass( 'out_left' );
+  $( '.menubutton#back' ).show();
+}
+
+function f_click_liste( e ) {
+  console.log( '> click @ li element' );
+  console.log( 'parameter e:' + e );
+  
+  // Erzeuge neue 'page' Section
+  $seite = $( '<section class="page out_right" id="detail"></section>' );
+  $( '.page#haupt' ).after( $seite );
+  
+  // Lade Detail-Seite, slide herein
+  $seite.load( 'detail.html #detail_inhalt', f_page_slide );
+}
+
+
 $( document ).ready( function() {
+  // Dokument ist fertig geladen
   console.log( "Hallo los geht's!" );
   
   // Binde KlickHandler an die Liste
-  $( '.list' ).on( 'click', 'li', function( e ) {
-    console.log( '> click @ li element' );
-    
-    // Erzeuge neue 'page' Section
-    $seite = $( '<section class="page out_right" id="detail"></section>' );
-    $( '.page#haupt' ).after( $seite );
-    
-    // Lade Detail-Seite, slide herein
-    $seite.load( 'detail.html #detail_inhalt', function( e ) {
-      console.log( '> complete @ $seite.load' );
-      // Seite ist geladen worden, starte Slide Animation
-      $seite.removeClass( 'out_right' );
-      $( '.page#haupt' ).addClass( 'out_left' );
-      $( '.menubutton#back' ).show();
-    } );
-    
-  } );
+  var $list, $titlebar;
+  $list = $( '.list' );
+  $list.on( 'click', 'li', f_click_liste );
   
   // Binde KlickHandler an die Menutaste
-  $( '.titlebar' ).on( 'click', '.menubutton#back', function( e ) {
-    console.log( '> click @ .menubutton#back' );
-    
-    // Schiebe Seiten wieder raus
-    $( '.page#haupt' ).removeClass( 'out_left' );
-    $( '.page#detail' ).addClass( 'out_right' );
-    $( '.menubutton#back' ).hide();
-  } );
+  $titlebar = $( '.titlebar' );
+  $titlebar.on( 'click', '.menubutton#back', f_page_slide_back );
   
 } );
